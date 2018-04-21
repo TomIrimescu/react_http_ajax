2fc9a7e59b8c7db82fc484b0e6bcc430a7a2281d
TomIrimescu <tom@irimescu.com>
2018-04-21 14:23:04 -0400

    added axios interceptors for requests/responses & base URL, default headers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(requestConfig => {
+  console.log("Request Config");
+  console.log(requestConfig);
+  // Edit request config
+  return requestConfig;
+}, error => {
+    console.log(error); // global log file to optionally send to a server
+    return Promise.reject(error); // forwards to request in component to be handled locally with the catch method
+});
+
+axios.interceptors.response.use(responseConfig => {
+  console.log("Response Config");
+  console.log(responseConfig);
+  // Edit request config
+  return responseConfig;
+}, error => {
+  console.log(error); // global log file to optionally send to a server
+  return Promise.reject(error); // forwards to request in component to be handled locally with the catch method
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
